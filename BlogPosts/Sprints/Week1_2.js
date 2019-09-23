c07ff747290be1679455550346c6f460de863a0d
gbbas <super.gbbas@users.noreply.github.com>
2019-09-24 01:16:54 +0200

    1

---
+++ BlogPosts/Sprints/Week1_2.js
@@ -0,0 +1,30 @@
+document.write(`
+    <div id="Week1_2.js" class="ABlogPost">
+
+    <h1>Week 1.2</h1>
+    <br>
+    <p> Week 1.2 is begonnen met een presentatie over hoe het de studie, en specifiek dit jaar, in elkaar zit. </p>
+    <p>Daarna kregen we een speedcourse in de Adafruit en MakeCode. Hierna zijn de meeste, waaronder ik ook,  meteen aan hun robot begonnen. Dit ging met hoge (en soms ook iets te hoge) ambitie. </p>
+    <p>Mijn idee voor mijn robot was een machine die rupsbanden gebruikt voor meer aanraking met de grond. Daarnaast wilde ik een soort schep aan de voorkant die ervoor zou zorgen dat van de  vijandelijke roboten hun voor banden niet meer de grond aan zouden raken. Hierdoor zou de tegenstander minder weerstand kunnen bieden. Hier is een tekening van mijn eerste idee.</p>
+    <img src="http://undaground.ga/uploads/BlogAssets/foto1-1.png" alt="RoboDiagram" class="responsive">
+    <p>Op de tweede dag is het meeste bouw werk gebeurd. Op deze dag heb ik niet alleen de banden gemaakt, maar ook een speedcourse Mosfet gekregen en een betere motor kunnen lenen. Hieronder is een foto van de betere motor. </p>
+    <img src="http://undaground.ga/uploads/BlogAssets/foto1-2.jpg" alt="Motor" class="responsive">
+    <p>Daarnaast was er ook wat simpele code nodig om de motor te later draaien.</p>
+    <img src="http://undaground.ga/uploads/BlogAssets/foto1-3.jpg" alt="Code" class="responsive">
+    <p>Uiteindelijk begon ik de tijdsdruk te voelen en heb ik besloten om van 8 wielen met een rupsband over te stappen naar maar 2 wielen. Hierdoor veranderde de schematische tekening van mijn robot hier naar.</p>
+    <img src="http://undaground.ga/uploads/BlogAssets/foto1-4.png" alt="RoboDiagramV2" class="responsive">
+    <p>En dit waren de banden zelf.</p>
+    <img src="http://undaground.ga/uploads/BlogAssets/foto1-5.jpg" alt="Wheel" class="responsive">
+    <p>Voor de battles moesten we onze robots ook pitchen. </p>
+    <video src="http://undaground.ga/uploads/BlogAssets/video1-1.mp4" controls class="responsive"></video>
+    <p>De eerste battle had mijn bot vrij makkelijk gewonnen maar de tweede was wat zwaarder. </p>
+    <video src="http://undaground.ga/uploads/BlogAssets/video1-2.mp4" controls class="responsive"></video>
+    <p>Met technische problemen aan bijde bots kon mijn bot het nog wat langer uithouden maar daarnaa toch verloren omdat mijn motor weg schoot.</p>
+    <p>Dit kwam doordat de assen van mijn wielen niet goed vast zaten aan de motor. Hier heb ik geen hete lijm gebruikt aangezien het een leen motor was.</p>
+    <p>Wat ik beter had kunnen doen is mijn tijd inplannen zodat ik eerder had gezien dat een rupsband maken te veel tijd kost en dus was ik eerder overgestapt op de twee wiel versie van mijn tekeningen.</p>
+    <p>Daarnaast had ik eigenlijk een langer stuk draad moeten halen zodat ik mijn batterij houder beter had kunnen modificeren. De draden waren zo kort dat ze afbraken. Dit heb ik weer aan elkaar gekregen met een paar krimpkousjes.</p>
+    <img src="http://undaground.ga/uploads/BlogAssets/foto1-6.jpg" alt="BatteryPack" class="responsive">
+    <p>En dan nu het zelf reflectie formulier(uit getyped voor betere leesbaarheid).</p>
+    <img src="http://undaground.ga/uploads/BlogAssets/WeekRapportage1_2.png" alt="week rapportage 1_2" class="responsive">
+    </div>
+`);
---
+++ BlogPosts/Sprints/Week1_3.js
@@ -0,0 +1,8 @@
+document.write('\
+\
+    <div id="Week1_3.js" class="BlogPostInactive">\
+\
+    <h1>Testing 2</h1>\
+\
+    </div>\
+');
