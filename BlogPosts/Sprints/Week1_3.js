7055abbfbf71669b6d72ab8df94653bf83d10419
gbbas <super.gbbas@users.noreply.github.com>
2019-09-24 10:54:16 +0200

    working on blog

--- BlogPosts/Sprints/Week1_3.js
+++ BlogPosts/Sprints/Week1_3.js
@@ -2,6 +2,9 @@ document.write(`
       <div id="Week1_3.js" class="ABlogPost CLE1 Sprint2">
 
       <h1>Week1_3</h1>
-      <p> Deze week zijn we Begonnen met sprint 2 van CLE1</p>
+      <p>Deze week zijn we Begonnen met sprint 2 van CLE1</p>
+      <p>Hiervoor moeten we een interactief smartobject maken voor een zorginstelling, winkel of museum.</p>
+      <p>Daarnaast moet het smartobject ook een probleem oplossen die mensen kunnen hebben binnen die contexten.</p>
+      <p></p>
       </div>
 `);
