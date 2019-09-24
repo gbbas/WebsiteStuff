cb909ba53f36ce3f02aa4d1a624edd074c9963a0
gbbas <super.gbbas@users.noreply.github.com>
2019-09-24 17:05:50 +0200

    Changes to Blog

--- BlogPosts/Sprints/Week1_3.js
+++ BlogPosts/Sprints/Week1_3.js
@@ -3,8 +3,13 @@ document.write(`
 
       <h1>Week1_3</h1>
       <p>Deze week zijn we Begonnen met sprint 2 van CLE1</p>
-      <p>Hiervoor moeten we een interactief smartobject maken voor een zorginstelling, winkel of museum.</p>
-      <p>Daarnaast moet het smartobject ook een probleem oplossen die mensen kunnen hebben binnen die contexten.</p>
-      <p></p>
+      <p>Hiervoor moeten we een interactief smartobject maken voor een zorginstelling, winkel of museum. Daarnaast moet het smartobject ook een probleem oplossen die mensen kunnen hebben binnen die contexten.</p>
+      <p>Wij zijn in een groepje op onderzoek uitgegaan maar dit heeft uiteindelijk weinig opgeleverd.</p>
+      <p>Eerst gingen we een beetje brainstormen om problemen te bedenken als voorbereiding op een mogelijk interview. Hieruit kwam dat we vooral naar winkels als context leunde.</p>
+      <p>We zijn bij het Natuur Historisch museum geweest voor meer ideeën maar toen ik vroeg of we wat snelle vragen konden stellen werdt tegen ons gezegt dat we daarvoor een afspraak zouden moeten maken.</p>
+      <p>Uiteindelijk zijn we op een paar plekken geweest met vragen maar leverde het allemaal weinig op.</p>
+      <p>Terug op school kwam een peercoach van een andere groep met het idee gestructueerd te brainstormen. Dit is daarna, bij de prompt van de docenten om te gaan brainstormen, verder uitgewerkt.</p>
+      <p>Hieruit is het volgende diagram gekomen die jasper heeft uitgewerkt terwijl donovan en ik de speedcourse stadslab volgden.</p>
+      <img src="uploads/BlogAssets/foto2-1.jpg" alt="diagram"></img>
       </div>
 `);
