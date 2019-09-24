6cc2fafd3dd57382f902e5ad9f04aef8f64c295c
gbbas <super.gbbas@users.noreply.github.com>
2019-09-24 09:50:16 +0200

    syntax fix

--- BlogPosts/Sprints/Week1_3.js
+++ BlogPosts/Sprints/Week1_3.js
@@ -1,7 +1,7 @@
 document.write(`
       <div id="Week1_3.js" class="ABlogPost CLE1 Sprint2">
 
-      <h1>Week1_3</h>
+      <h1>Week1_3</h1>
       <p> Deze week zijn we Begonnen met sprint 2 van CLE1</p>
       </div>
 `);
